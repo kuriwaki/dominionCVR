2eb010951b1e59941e9c6e6801dc00d6c72995e3
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-08-19 14:25:23 -0400

    Modularize functions

--- R/readCVR.R
+++ R/readCVR.R
@@ -5,61 +5,87 @@
 #'  or use `path` to give the `fromJSON` path directly.
 #'
 #' @importFrom jsonlite fromJSON
-#' @importFrom purrr map_dfr
+#' @importFrom purrr map map_dfr
+#' @importFrom furrr future_map_dfr
+#' @importFrom fs path_file
 #' @importFrom magrittr %>%
 #' @examples
-#'  extract_cvr(path = "data-raw/json/CvrExport_42.json")
+#'
+#'  library(furrr)
+#'  plan("multicore")
+#'  extract_cvr(path = c("data-raw/json/CvrExport_42.json", "data-raw/json/CvrExport_24940.json"))
 #'
 #' @export
 #'
-extract_cvr <- function(cvr = NULL, path = NULL) {
+extract_cvr <- function(cvr = NULL, path = NULL, verbose = TRUE) {
 
   if (is.null(cvr) & is.null(path))
     stop("Must have an object in `cvr` or a path in `path`")
 
-  if (is.null(cvr) & !is.null(path))
-    cvr <- fromJSON(path, simplifyDataFrame = FALSE)
+  if (is.null(cvr) & !is.null(path)) {
+    cvr <- map(path,
+                ~fromJSON(.x, simplifyDataFrame = FALSE)
+    )
+  }
+
+  # list of cvrExports to list of files (which include sessions)
+  data <- map(cvr, ~ .x$Sessions)
+  names(data) <- fs::path_file(path)
+
+  # output
+  future_map_dfr(data,
+                 .extract_from_file,
+                 .id = "file",
+                 .progress = verbose)
+}
+
+#' @keywords internal
+.extract_from_file <- function(file) {
+  future_map_dfr(file, ~ .extract_from_session(.x))
+}
+
+#' @keywords internal
+.extract_from_session <- function(sess) {
+    map_dfr(sess$Original$Cards, ~.extract_from_card(.x, sess))
+}
+
+#' @keywords internal
+.extract_from_card <- function(card, sess) {
+  map_dfr(card$Contests, ~.extract_from_contest(.x, sess, card))
+}
+
+#' @keywords internal
+.extract_from_contest <- function(cont, sess, card) {
+    map_dfr(cont$Marks, ~.extract_from_mark(.x, sess, card, cont))
+}
 
 
-  data <- cvr$Sessions
-
-  data %>%
-    map_dfr(function(sess) {
-      precinct <- sess$Original$PrecinctPortionId
-      ballotType <- sess$Original$BallotTypeId
-      sess$Original$Cards %>%
-        map_dfr(function(card) {
-          map_dfr(card$Contests, function(cont) {
-            # print(names(card))
-            map_dfr(cont$Marks, function(m) {
-              tibble(
-                # file_name = fn, # if only path, add the filename
-                precinct = precinct,
-                ballotType = ballotType,
-                tabulator = sess$TabulatorId,
-                batch = sess$BatchId,
-                recordId = sess$RecordId,
-                countyGroupId = sess$CountingGroupId,
-                # #imageMask = sess$ImageMask,
-                sessionType = sess$SessionType,
-                votingSessionId = sess$VotingSessionIdentifier,
-                uniqueVotingIdentifer = sess$UniqueVotingIdentifier,
-                cardId = card$Id,
-                # # keyinId = card$KeyInId,
-                paperIndex = card$PaperIndex,
-                contestId = cont$Id,
-                # contest = contests[as.character(cont$Id)],
-                overvotes = cont$Overvotes,
-                undervotes = cont$Undervotes,
-                candidateID = m$CandidateId,
-                # candidate = cands[as.character(m$CandidateId)],
-                rank = m$Rank,
-                mdens = m$MarkDensity,
-                isambig = m$IsAmbiguous,
-                isvote = m$IsVote
-              )
-            })
-          })
-        })
-    })
+#' @keywords internal
+.extract_from_mark <- function(mark, sess, card, cont) {
+  tibble(
+    # file_name = fn, # if only path, add the filename
+    precinct = sess$Original$PrecinctPortionId,
+    ballotType = sess$Original$BallotTypeId,
+    tabulator = sess$TabulatorId,
+    batch = sess$BatchId,
+    recordId = sess$RecordId,
+    countyGroupId = sess$CountingGroupId,
+    # #imageMask = sess$ImageMask,
+    sessionType = sess$SessionType,
+    votingSessionId = sess$VotingSessionIdentifier,
+    uniqueVotingIdentifer = sess$UniqueVotingIdentifier,
+    cardId = card$Id,
+    # # keyinId = card$KeyInId,
+    paperIndex = card$PaperIndex,
+    contestId = cont$Id,
+    # contest = contests[as.character(cont$Id)],
+    overvotes = cont$Overvotes,
+    undervotes = cont$Undervotes,
+    candidateID = mark$CandidateId,
+    # candidate = cands[as.character(m$CandidateId)],
+    rank = mark$Rank,
+    mdens = mark$MarkDensity,
+    isambig = mark$IsAmbiguous,
+    isvote = mark$IsVote
+  )
 }
